885f4ca6759f018cddc4f60bd7ec7ce1e2b9d5b8
MuhammadRifki03 <83215587+MuhammadRifki03@users.noreply.github.com>
2021-05-04 08:56:51 +0700

    Add files via upload

--- Tugas-6-ES6/index.js
+++ Tugas-6-ES6/index.js
@@ -1,1 +1,39 @@
+//SOAL 2
+const newFunction = function literal(firstName, lastName){
+    return {
+      const firstName = 'William'
+      const lastName = 'Imoh'
 
+      const fullName = '${firstName} ${lastName}'
+        console.log(fullname)
+      }
+    }
+  }
+
+  const newFunction = ({firstName}, {lastName})
+
+
+//SOAL 3
+const newObject = {
+    firstName: "Muhammad",
+    lastName: "Iqbal Mubarok",
+    address: "Jalan Ranamanyar",
+    hobby: "playing football",
+  }
+const {firstName, lastName, address, hobby} = newObject
+
+console.log(firstName, lastName, address, hobby)
+
+//SOAL 4
+let west = ["Will", "Chris", "Sam", "Holly"]
+let east = ["Gill", "Brian", "Noel", "Maggie"]
+let combined = [...west, ...east]
+console.log(combined)
+
+
+//SOAL 5
+const planet = 'earth'
+const view = 'glass'
+
+const before = '${planet} ${view}'
+console.log(before)
